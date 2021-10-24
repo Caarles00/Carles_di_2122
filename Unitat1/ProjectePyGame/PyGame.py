b3dedf22969cf214c6caecdf79820a8ddd0423a7
Caarles00 <carlesmonti@gmail.com>
2021-10-25 00:18:47 +0200

    DI

--- Unitat1/ProjectePyGame/PyGame.py
+++ Unitat1/ProjectePyGame/PyGame.py
@@ -1,9 +1,13 @@
 # Import random for random numbers
 import random
 import os
+import time
 # Import the pygame module
 import pygame
 from pygame.constants import RLEACCEL
+
+ruta_base = os.path.dirname(__file__)
+ruta_a_recurs = os.path.join(ruta_base, "Utils")
 
 # Import pygame.locals for easier access to key coordinates
 # Updated to conform to flake8 and black standards
@@ -21,12 +25,16 @@ QUIT
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-#ruta_base = os.path.dirname(_file_)
-#ruta_a_recurs = os.path.join(ruta_base, "DI/Utils")
-
 # Define a player object by extending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player'
 class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load(os.path.join(ruta_a_recurs, "jet.png")).convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect()
+
     # Move the sprite based on user keypresses
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
@@ -47,38 +55,21 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
-
-    try:
-        ruta_base = os.path.dirname(_file_)
-        ruta_a_recurs = os.path.join(ruta_base, "jet.png")
-
-        def _init_(self):
-            super(Player, self)._init_()
-            self.surf = pygame.image.load("jet.png").convert()
-            self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-            self.rect = self.surf.get_rect()
-    except FileExistsError:
-        print("El fitxer no existeix")
-
+    
 # Define the enemy object by extending pygame.sprite.Sprite
 # The surface you draw on the screen is now an attribute of 'enemy'
 class Enemy(pygame.sprite.Sprite):
-    try:
-        ruta_base = os.path.dirname(_file_)
-        ruta_a_recurs = os.path.join(ruta_base, "missile.png")
-        def _init_(self):
-            super(Enemy, self)._init_()
-            self.surf = pygame.image.load("missile.png").convert()
-            self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-            self.rect = self.surf.get_rect(
-                center=(
-                    random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                    random.randint(0, SCREEN_HEIGHT),
-                )
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load(os.path.join(ruta_a_recurs, "missile.png")).convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),                    random.randint(0, SCREEN_HEIGHT),
             )
-            self.speed = random.randint(5, 20)
-    except FileExistsError:
-        print("El fitxer no existeix")
+        )
+        self.speed = random.randint(5, 20)
+    
 
     # Move the sprite based on speed
     # Remove the sprite when it passes the left edge of the screen
@@ -88,25 +79,23 @@ class Enemy(pygame.sprite.Sprite):
             self.kill()
 
 class Cloud(pygame.sprite.Sprite):
-    try:
-        ruta_base = os.path.dirname(_file_)
-        ruta_a_recurs = os.path.join(ruta_base, "cloud.png")
-
-        def _init_(self):
-            super(Cloud, self)._init_()
-            self.surf = pygame.image.load("cloud.png").convert()
-            self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-            self.rect = self.surf.get_rect(
-                center=(
-                    random.randint(SCREEN_WIDTH + 50, SCREEN_HEIGHT + 100),
-                    random.randint(0, SCREEN_HEIGHT)
-                )
-            )
-    except FileExistsError:
-        print("El fitxer no existeix")
+    
+    def __init__(self):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load(os.path.join(ruta_a_recurs, "cloud.png")).convert()
+        self.surf.set_colorkey((0, 0, 0), RLEACCEL)            
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT)
+            )            
+        )
+    
 
     def update(self):
-        self.rect.move_ip(-self.speed, 5)
+        self.rect.move_ip(-5 ,0)
+        if self.rect.right < 0:
+            self.kill()
 
 #Setupforsounds. Defaultsaregood.
 pygame.mixer.init()
@@ -115,14 +104,15 @@ pygame.mixer.init()
 pygame.init()
 
 #Load and play background music
-pygame.mixer.music.load("Apoxode_-_Electric_1.ogg")
+pygame.mixer.music.load(os.path.join(ruta_a_recurs, "Apoxode_-_Electric_1.ogg"))
 pygame.mixer.music.play(loops=-1)
 
 #Load all sound files
 #Soundsources:Jon Fincher
-move_up_sound=pygame.mixer.Sound("Rising_putter.ogg")
-move_down_sound=pygame.mixer.Sound("Falling_putter.ogg")
-collision_sound=pygame.mixer.Sound("Collision.ogg")
+move_up_sound=pygame.mixer.Sound(os.path.join(ruta_a_recurs, "Rising_putter.ogg"))
+move_down_sound=pygame.mixer.Sound(os.path.join(ruta_a_recurs, "Falling_putter.ogg"))
+collision_sound=pygame.mixer.Sound(os.path.join(ruta_a_recurs, "Collision.ogg"))
+
 
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
@@ -132,6 +122,10 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 250)
 
+
+ADDCLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDCLOUD, 1000)
+
 #Instantiate player. Right now, this is just a rectangle.
 player = Player()
 
@@ -139,6 +133,7 @@ player = Player()
 # - enemies is used for collision detection and position updates
 # - all_sprites is used for rendering
 enemies = pygame.sprite.Group()
+clouds = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
@@ -167,6 +162,13 @@ while running:
             new_enemy = Enemy()
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
+        
+        # Add a new cloud?
+        elif event.type == ADDCLOUD:
+            # Create the new cloud and add it to sprite groups
+            new_cloud = Cloud()
+            clouds.add(new_cloud)
+            all_sprites.add(new_cloud)
 
     # Get all the keys currently pressed
     pressed_keys = pygame.key.get_pressed()
@@ -177,8 +179,11 @@ while running:
     # Update enemy position
     enemies.update()
 
+    # Update cloud position
+    clouds.update()
+
     # Fill the screen with black
-    screen.fill((0, 0, 0))
+    screen.fill((135, 206, 250))
 
     # Draw all sprites
     for entity in all_sprites:
@@ -187,6 +192,8 @@ while running:
     # Check if any enemies have collided with the player
     if pygame.sprite.spritecollideany(player, enemies):
         # If so, then remove the player and stop the loop
+        collision_sound.play()
+        time.sleep(1)
         player.kill()
         running = False
 
